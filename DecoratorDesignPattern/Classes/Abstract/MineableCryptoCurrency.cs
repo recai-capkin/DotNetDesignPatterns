4f71dc00b7c2f4f8098c32c19d9b821cc85809de
Recai-Capkin <recai.capkin28@gmail.com>
2022-08-19 23:59:15 +0300

    Decorator DP abstract class yazıldı.

---
+++ DecoratorDesignPattern/Classes/Abstract/MineableCryptoCurrency.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorDesignPattern.Classes.Abstract
+{
+    abstract class MineableCryptoCurrency
+    {
+        public string name;
+        public abstract void Mine();
+    }
+}
