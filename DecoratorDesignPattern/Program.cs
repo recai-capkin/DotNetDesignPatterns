c8a5a090610522989b9aa3c1c70aa0d5485c91ab
Recai-Capkin <recai.capkin28@gmail.com>
2023-12-05 18:39:44 +0300

    Some design patterns added and some fix

--- DecoratorDesignPattern/Program.cs
+++ DecoratorDesignPattern/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using DecoratorDesignPattern.Classes.Abstract.Decorator;
+using DecoratorDesignPattern.Classes.Decorator;
+using DecoratorDesignPattern.Classes;
+using DecoratorDesignPattern.Classes.Abstract;
+
+CryptoCurrency myCurrency = new MineableCurrency();
+
+// Madencilik işlevlerini eklemek için dekoratör kullanma
+CryptoCurrencyDecorator decoratedCurrency = new MineableCurrencyDecorator(myCurrency);
+
+// İşlemleri gerçekleştirme
+decoratedCurrency.Send(100, "wallet12345");
+
+MineableCurrencyDecorator decoratedCurrencyMineable = new MineableCurrencyDecorator(myCurrency);
+decimal newDifficulty = decoratedCurrencyMineable.changeDifficulty();
+Console.WriteLine($"Yeni zorluk derecesi: {newDifficulty}");
+
+// Blok boyutunu değiştirme
+decimal newBlockSize = decoratedCurrencyMineable.changeBlockSize(50000); // Örnek blok boyutu artışı
+Console.WriteLine($"Yeni blok boyutu: {newBlockSize} bytes");
+
+// Madencilik işlemi
+string miningResult = ((MineableCurrency)myCurrency).RunMining(1000); // Örnek hash oranı
+Console.WriteLine(miningResult);
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 <H1>Design Patterns</H1>
-
+<br>
 <H2>Creational Patterns</H2>
 <H2>Structural Pattern</H2>
 <H2>Behavioral Pattern</H2>
